b19a3f5c7044c276818207d22048c34151b4e0af
SunitMaveric17 <sunitp@maveric-systems.com>
2023-11-09 11:40:14 +0530

    Cors Config

--- src/main/java/org/maveric/quarkus/panache/config/CorsFilter.java
+++ src/main/java/org/maveric/quarkus/panache/config/CorsFilter.java
@@ -3,9 +3,11 @@ package org.maveric.quarkus.panache.config;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
 
 import java.io.IOException;
 
+@Provider
 public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
