f9bd6646daa5b2216d16f9385a573e245fd2c664
MeletoSofiya <meletos@maveric-systems.com>
2023-11-17 17:28:58 +0530

    get abi bug fix  added

--- src/main/java/org/maveric/quarkus/panache/repository/SavingsAccountRepository.java
+++ src/main/java/org/maveric/quarkus/panache/repository/SavingsAccountRepository.java
@@ -9,11 +9,19 @@ import java.util.List;
 @ApplicationScoped
 public class SavingsAccountRepository implements PanacheRepository<SavingsAccount> {
 
-    public List<SavingsAccount> findBySavingsAccountId(Long savingsAccountId){
+    public SavingsAccount findBySavingsAccountId(Long savingsAccountId){
+        return find("savingsAccountId", savingsAccountId).firstResult();
+    }
+
+    public  SavingsAccount findByCustomerId(Long customerId){
+        return find("customerId", customerId).firstResult();
+    }
+
+    public List<SavingsAccount> findBySavingsAccountListById(Long savingsAccountId){
         return find("savingsAccountId", savingsAccountId).list();
     }
 
-    public  List<SavingsAccount> findByCustomerId(Long customerId){
+    public  List<SavingsAccount> findByCustomerListById(Long customerId){
         return find("customerId", customerId).list();
     }
 }
